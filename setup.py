ac264e026944bc1dc8c756b0e5a34cabcb00ddcc
Russell Keith-Magee <russell@keith-magee.com>
2018-06-20 09:54:34 +0800

    Bumped version for v0.1.3 release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-tldr',
-    version='0.1.2',
+    version='0.1.3',
     author='Russell Keith-Magee',
     author_email='russell@keith-magee.com',
     maintainer='Russell Keith-Magee',
