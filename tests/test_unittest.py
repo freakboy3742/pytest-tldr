7ed38a9bbd1fedd9747c886e567ba9221af8de7c
Russell Keith-Magee <russell@keith-magee.com>
2022-10-26 08:02:55 +0800

    Blacken test code.

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -2,63 +2,69 @@ import sys
 
 
 def test_pass(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
         class TestCase(unittest.TestCase):
             def test_pass(self):
                 self.assertTrue(str(True) == 'True')
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_pass ... ok'
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(["*::TestCase::test_pass ... ok"])
 
     assert result.ret == 0
 
 
 def test_fail(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
         class TestCase(unittest.TestCase):
             def test_fail(self):
                 self.assertTrue(str(True) == 'Garble')
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_fail ... FAIL',
-        'FAIL: test_fail.py::TestCase::test_fail'
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        ["*::TestCase::test_fail ... FAIL", "FAIL: test_fail.py::TestCase::test_fail"]
+    )
 
     assert result.ret == 1
 
 
 def test_error(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
         class TestCase(unittest.TestCase):
             def test_error(self):
                 raise Exception("This shouldn't happen")
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_error ... ERROR',
-        'ERROR: test_error.py::TestCase::test_error'
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        [
+            "*::TestCase::test_error ... ERROR",
+            "ERROR: test_error.py::TestCase::test_error",
+        ]
+    )
 
     assert result.ret == 1
 
 
 def test_skip(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
@@ -66,18 +72,22 @@ def test_skip(testdir):
             @unittest.skip('this should be skipped')
             def test_skip(self):
                 self.assertTrue(str(True) == 'Garble')
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_skip ... Skipped: this should be skipped',
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        [
+            "*::TestCase::test_skip ... Skipped: this should be skipped",
+        ]
+    )
 
     assert result.ret == 0
 
 
 def test_xfail(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
@@ -85,18 +95,22 @@ def test_xfail(testdir):
             @unittest.expectedFailure
             def test_expected_failure(self):
                 self.assertTrue(str(True) == 'Garble')
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_expected_failure ... expected failure',
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        [
+            "*::TestCase::test_expected_failure ... expected failure",
+        ]
+    )
 
     assert result.ret == 0
 
 
 def test_upass(testdir):
-    testdir.makepyfile("""
+    testdir.makepyfile(
+        """
         import unittest
 
 
@@ -104,12 +118,15 @@ def test_upass(testdir):
             @unittest.expectedFailure
             def test_unexpected_success(self):
                 self.assertTrue(str(True) == 'True')
-    """)
+    """
+    )
 
-    result = testdir.runpytest('-v')
-    result.stdout.fnmatch_lines([
-        '*::TestCase::test_unexpected_success ... unexpected success',
-    ])
+    result = testdir.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        [
+            "*::TestCase::test_unexpected_success ... unexpected success",
+        ]
+    )
 
     # pytest under Python2 reports an unexpected pass as a success,
     # but a failure under Python3.
