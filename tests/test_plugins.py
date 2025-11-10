861b6a06575e764d751eee4aa7929b21cee7e32e
Konrad Weihmann <46938494+priv-kweihmann@users.noreply.github.com>
2025-11-10 23:57:15 +0100

    Adapt to API deprecation in pytest 9.x (#39)
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -10,7 +10,7 @@ def test_coverage(testdir):
     result.stdout.fnmatch_lines(
         [
             "test_coverage.py::test_coverage ... ok",
-            "*----- coverage: platform *-----",
+            "* coverage: platform *",
             "Name               Stmts   Miss  Cover",
             "--------------------------------------",
             "test_coverage.py       2      0   100%",
